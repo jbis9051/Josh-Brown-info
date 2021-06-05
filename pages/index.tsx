c56547e8deb7599f335e0c6582a9fab57564d8fa
jbis9051 <josh9051@gmail.com>
2021-06-05 14:18:23 -0400

    Gradient thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,14 @@ export default function Home() {
             </Head>
             <div className={styles.container}>
                 <div className={styles.subContainer}>
-                    <div className={styles.imageWrapper}>
-                        <img
-                            className={styles.image}
-                            src={'/assets/images/me/profile.png'}
-                            alt={'Profile Picture'}
-                        />
+                    <div className={styles.imageBorder}>
+                        <div className={styles.imageWrapper}>
+                            <img
+                                className={styles.image}
+                                src={'/assets/images/me/profile.png'}
+                                alt={'Profile Picture'}
+                            />
+                        </div>
                     </div>
                     <span className={styles.subText}>
                         Hello. I'm Josh, a student interested in cryptography.
